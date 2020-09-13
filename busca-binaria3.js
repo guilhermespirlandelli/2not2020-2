95d03062a667ea9cff854475c81c917dcbe9c67a
guilhermespirlandelli <guilherme.spirlandelli01@fatec.sp.gov.br>
2020-09-13 23:03:02 +0000

    (10/09) Busca binária 3

--- busca-binaria3.js
+++ busca-binaria3.js
@@ -2,12 +2,15 @@
 //pelo critério de busca
 let comp = 0
 
-// Implementação Iterativa
-function buscaBinaria(lista, valorBusca, fnComp) {
-    let inicio = 0
-    let fim = lista.length - 1
+// Implementação recursiva
+// Uma função chama a si mesma com pelo menos um valor alterado.
+// Toda função recursiva precisa ter pelo menos uma condição de saída.
 
-    while (fim >= inicio) {
+// Os parêmetros inicio e fim são OPCIONAIS. caso a função seja chamada sem especificá-los,
+// eles assumirão o valor indicado.
+function buscaBinaria(lista, valorBusca, fnComp, inicio = 0, fim = lista.length - 1) {
+
+    if(fim >= inicio) {
         //Math.floor(): retira as casas decimais de um número
         let meio = Math.floor((fim + inicio) / 2)
 
@@ -16,17 +19,20 @@ function buscaBinaria(lista, valorBusca, fnComp) {
         // Verifica se o valor na posição média é o valor de busca.
         if (res == 0) {
             comp++
-            return meio
+            return meio //condição de saída
         }
         else if (res < 0) {
             comp += 2
-            fim = meio - 1
+            //fim = meio - 1
+            return buscaBinaria(lista, valorBusca, fnComp, inicio, meio -1)
         }
         else { // res > 0
             comp += 2
-            inicio = meio + 1
+            //inicio = meio + 1
+            return buscaBinaria(lista, valorBusca, fnComp, meio +1, fim)
         }
     }
+    //condição de saída
     return -1 //Valor não encontrado
 
 }
@@ -39,6 +45,17 @@ function comparaNome(obj, valorBusca) {
     // Um número positivo para indicar que o primeiro > segundo
     else return 1 // valorBusca > obj.first_name
 }
+
+let nums = [4, 16, 22, 29, 35, 44, 52, 58, 66, 71, 80, 88, 94]
+
+console.log(buscaBinaria(nums, 66, (elPos, busca) => {
+    if(busca === elPos) return 0
+    else if(busca < elPos) return -1
+    else return 1
+}))
+
+// Termina a execução sem passar pelas linhas abaixo
+//process.exit(0) // 0 = saindo ok, sem erros
 
 let listaNomes = require('./dados/lista-nomes')
 
