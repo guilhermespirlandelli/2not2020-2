b56956e479c64431d06d41cd7e9205dd8cf121eb
guilhermespirlandelli <guilherme.spirlandelli01@fatec.sp.gov.br>
2020-09-27 20:22:50 +0000

    (24/09) Selection Sort com função de comparação

--- selection-sort2.js
+++ selection-sort2.js
@@ -1,6 +1,12 @@
+/*
+    Função de comparação fnComp
+    - Recebe dois valores para comparação
+    - Retorno:
+        - true se o primeiro valor for maior que o segundo
+        - false caso contrário
+*/
 
-
-function selectionSort(vetor) {
+function selectionSort(vetor, fnComp) {
     let passadas = 0, comparacoes = 0, totalTrocas = 0
 
     //Função que encontra o menor valor em um segmento de vetor (subvetor)
@@ -8,7 +14,8 @@ function selectionSort(vetor) {
     function encontrarMenor(vetor, inicio) {
         let posMenor = inicio
         for(let i = inicio + 1; i < vetor.length; i++) {
-            if(vetor[i] < vetor[posMenor]) posMenor = i
+            //if(vetor[i] < vetor[posMenor]) posMenor = i
+            if(! fnComp(vetor[i], vetor[posMenor])) posMenor = i
             comparacoes++
         }
         return posMenor
@@ -25,7 +32,8 @@ function selectionSort(vetor) {
         //Caso o valor encontrado seja menor que o valor atual,
         //procede-se à troca
         comparacoes++
-        if(vetor[posMenor] < vetor[i]) {
+      // if(vetor[posMenor] < vetor[i]) {
+         if(fnComp(vetor[i], vetor[posMenor])) { //Ordem dos argumentos invertida. 
             [vetor[posMenor], vetor[i]] = [vetor[i], vetor[posMenor]]
             totalTrocas++
         }
@@ -35,21 +43,14 @@ function selectionSort(vetor) {
 
 const nums = [56, 78, 44, 23, 99, 14, 60, 37, 6, 82, 31, 65]
 
-/*
-console.time('Nums')
-selectionSort(nums)
-console.timeEnd('Nums')
 
-console.log(nums)
-*/
-
-let nomes = require('./dados/100-mil-nomes (1)')
-console.time('teste nomes')
-selectionSort(nomes)
-console.timeEnd('teste nomes')
+let canditados = require('./dados/candidatos-2018')
+console.time('teste candidatos')
+selectionSort(canditados, (x, y) => x.NM_CANDIDATO > y.NM_CANDIDATO)
+console.timeEnd('teste candidatos')
 //Medindo a memória que foi utilizada pelo programa
 let memoria = process.memoryUsage().heapUsed / 1024 / 1024
-console.log(nomes)
+console.log(canditados)
 console.log('Memoria utilizada (MB)', memoria)
 
 
